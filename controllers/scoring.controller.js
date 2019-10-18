4f02b2953bf73220b75013f910ecd1f5c628b723
relifevn <thinhhuynh3108@gmail.com>
2019-10-18 22:17:27 +0700

    update routes

--- controllers/scoring.controller.js
+++ controllers/scoring.controller.js
@@ -1,10 +1,75 @@
 const API_KEY = process.env.API_KEY;
 
-const scoringTransactionData = require('../data/scorechain-bitcoin-get-scoring-transaction.json');
-const scoringAddressData = require('../data/scorechain-bitcoin-get-scoring-address.json');
-const scoringEntityData = require('../data/scorechain-bitcoin-get-scoring-entity.json');
-const scoringUtxosData = require('../data/scorechain-bitcoin-post-scoring-utxos.json');
+const scoreChainData = {
+  alerts: {
+    getAlerts: require('../data/scorechain/alerts/get-alerts.json'),
+  },
+  blockchain: {
+    address: require('../data/scorechain/blockchain-data/address.json'),
+    entity: require('../data/scorechain/blockchain-data/entity.json'),
+    status: require('../data/scorechain/blockchain-data/status.json'),
+    tx: require('../data/scorechain/blockchain-data/tx.json'),
+  },
+  reports: {
+    getReport: require('../data/scorechain/reports/get-report.json'),
+  },
+  scoring: {
+    address: require('../data/scorechain/scoring/address.json'),
+    entity: require('../data/scorechain/scoring/entity.json'),
+    transaction: require('../data/scorechain/scoring/transaction.json'),
+    utxos: require('../data/scorechain/scoring/utxos.json'),
+  },
+  customisation: {
+    scx: require('../data/scorechain/customisation/scx.json')
+  }
+}
 
+/* BLOCKCHAIN DATA */
+exports.blockchainStatus = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.blockchain.status);
+}
+
+exports.blockchainTx = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.blockchain.tx);
+}
+
+exports.blockchainAddress = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.blockchain.address);
+}
+
+exports.blockchainEntity = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.blockchain.entity);
+}
+
+/* SCORING */
 exports.transaction = (req, res) => {
   console.log(req.params, req.query, req.body);
 
@@ -13,7 +78,7 @@ exports.transaction = (req, res) => {
     return;
   }
 
-  res.json(scoringTransactionData);
+  res.json(scoreChainData.scoring.transaction);
 }
 
 exports.address = (req, res) => {
@@ -24,7 +89,7 @@ exports.address = (req, res) => {
     return;
   }
 
-  res.json(scoringAddressData);
+  res.json(scoreChainData.scoring.address);
 }
 
 exports.entity = (req, res) => {
@@ -35,7 +100,7 @@ exports.entity = (req, res) => {
     return;
   }
 
-  res.json(scoringEntityData);
+  res.json(scoreChainData.scoring.entity);
 }
 
 exports.utxos = (req, res) => {
@@ -46,7 +111,43 @@ exports.utxos = (req, res) => {
     return;
   }
 
-  res.json(scoringUtxosData);
+  res.json(scoreChainData.scoring.utxos);
+}
+
+/* CUSTOMISATION */
+exports.customisationScx = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.customisation.scx);
+}
+
+/* REPORTS */
+exports.getReport = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.reports.getReport);
+}
+
+/* ALERTS */
+exports.getAlerts = (req, res) => {
+  console.log(req.params, req.query, req.body);
+
+  if (req.query.token !== API_KEY) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  res.json(scoreChainData.alerts.getAlerts);
 }
 
 exports.scoringValidator = (req, res, next) => {
--- routes/scoring.router.js
+++ routes/scoring.router.js
@@ -1,9 +1,25 @@
 const router = require('express').Router()
   , controllers = require('../controllers')
 
+/* Blockchain data */
+router.get('/status', controllers.scoring.blockchainStatus);
+router.get('/tx/:hash', controllers.scoring.blockchainTx);
+router.get('/address/:address', controllers.scoring.blockchainAddress);
+router.get('/entity/:address', controllers.scoring.blockchainEntity);
+
+/* Scoring */
 router.get('/transaction/:input/:hash', controllers.scoring.transaction);
 router.get('/address/:type/:address', controllers.scoring.address);
 router.get('/entity/:type/:address', controllers.scoring.entity);
 router.post('/utxos/:type', controllers.scoring.utxos);
+
+/* Customisation */
+router.get('/scx', controllers.scoring.customisationScx);
+
+/* Reports */
+router.get('/report', controllers.scoring.getReport);
+
+/* Alerts */
+router.get('/alerts', controllers.scoring.getAlerts);
 
 module.exports = router;
